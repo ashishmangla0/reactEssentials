b71431dc4d1b731cebf8691a20067c399823f90d
ashishmg <ashishmangla1991@gmail.com>
2021-01-03 22:23:55 +0530

    add new color

--- src/index.js
+++ src/index.js
@@ -23,8 +23,9 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
- React.createElement("h1",null,"Heyyyy Everyone"),
-  document.getElementById('root')
+ //React.createElement("h1",null,"Heyyyy Everyone"),
+ React.createElement("h1",{style:{color:"blue"}},"Heyyyy Everyone"), 
+ document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
