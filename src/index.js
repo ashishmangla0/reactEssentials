66e3553a417cd4f4b4df541bcd4c772ea20579c9
ashish <ashishmangla1991@gmail.com>
2021-01-04 21:36:36 +0530

    add fragment

--- src/index.js
+++ src/index.js
@@ -4,9 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+function AppTwo(){
+    return(
+        <h1>this is second app</h1>
+    )
+}
+
 ReactDOM.render(
   <React.StrictMode>
+  <>
     <App />
+    <AppTwo/>
+    </>
   </React.StrictMode>,
   document.getElementById('root')
 );
