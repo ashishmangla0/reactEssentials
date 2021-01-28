57437ad26747c0bf4c1c54f023b6200762800f4d
ashish <ashishmangla1991@gmail.com>
2021-01-28 21:23:58 +0530

    useEfect

--- src/App.js
+++ src/App.js
@@ -1,38 +1,18 @@
 import './App.css';
-import {useState} from "react";
+import {useState,useEffect} from "react";
 
 function App() {
 
-  // multiple states
-  const [year,setYear] = useState(2050);
-  const [manager,setManager] = useState("Ashish");
-  const [status,setStatus] = useState("open");
- //first one is  state and another one is function
-  const handleManager = () => {
-          setManager("surbhi")
-  }
+  // useEfffect
+  const [checked,setChecked] = useState(false);
+useEffect(() => {
+  alert(`checked: ${checked.toString()}`)
+})
+
  return(
     <>
-    <div>
-      <h1> Manager on duty: {manager} </h1>
-      <button onClick={()=>handleManager()}>
-        change manager
-      </button>
-    </div>
-    <div>
-    <h2> status: {status} </h2>
-    <button onClick={()=> setStatus("open")}>
-      open
-    </button>
-    <button onClick={()=> setStatus("back in 5")}>
-      Break
-    </button>
-    <button onClick={()=> setStatus("closed")}>closed</button>
-    </div>
-    <div>
-      {year}
-      <button onClick={()=> setYear(year + 1)}>add year</button>
-    </div>
+    <input type="checkbox" value={checked} onChange={() => setChecked(!checked)}/>
+    {checked ? "checked": "not checked"}
 
     </>
   )
